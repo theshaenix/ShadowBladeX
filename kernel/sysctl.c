4afc748e28c9c4e9270837db3943ce598960bbc2
theshaenix <theshaenix@gmail.com>
2025-09-12 21:08:17 +0530

    sched/fair: bore: Implement sched_burst_exclude_kthreads sched_burst_exclude_kthreads was introduced in the newer version of sched BORE. This feature is trivial to implement but is a handy one.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -146,6 +146,7 @@ static int six_hundred_forty_kb = 640 * 1024;
 static int two_hundred_fifty_five = 255;
 
 #ifdef CONFIG_SCHED_BORE
+extern uint sched_burst_exclude_kthreads;
 extern uint sched_burst_smoothness_long;
 extern uint sched_burst_smoothness_short;
 extern uint sched_burst_fork_atavistic;
@@ -787,6 +788,15 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &two_hundred_fifty_five,
 	},
 #ifdef CONFIG_SCHED_BORE
+	{
+		.procname	= "sched_burst_exclude_kthreads",
+		.data		= &sched_burst_exclude_kthreads,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler = proc_douintvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
 	{
 		.procname	= "sched_burst_smoothness_long",
 		.data		= &sched_burst_smoothness_long,
